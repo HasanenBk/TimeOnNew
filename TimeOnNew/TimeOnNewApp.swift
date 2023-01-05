746124c211a1f88be7ccbd49d6bc12f6e7920f1c
Hasanen <hasanen.bak@gmail.com>
2023-01-05 01:24:03 +0100

    Initial Commit

---
+++ TimeOnNew/TimeOnNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeOnNewApp.swift
+//  TimeOnNew
+//
+//  Created by Hasanen B on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct TimeOnNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
